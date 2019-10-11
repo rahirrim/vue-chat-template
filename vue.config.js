ba5c5a9321e0e155e673eb9d8aba17571c19d10f
Рахимжан Иминов <riminov@alfabank.kz>
2019-10-12 03:23:42 +0600

    master some fix

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-// module.exports = {
-//     publicPath: process.env.NODE_ENV === 'production'
-//         ? '/ip-demo/'
-//         : '/'
-// };
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/ip-demo/'
+        : '/'
+};
